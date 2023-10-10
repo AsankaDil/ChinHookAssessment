30f4732b0eaf71b97187d32d83750b518a672c5b
AsankaDil <32638689+AsankaDil@users.noreply.github.com>
2023-10-10 14:51:05 +0530

    DBChanges

--- ChinHookInfranstructure/Context/ChinHookDbContext.cs
+++ ChinHookInfranstructure/Context/ChinHookDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ChinHookInfranstructure.Context
 {
-    public partial class ChinHookDbContext : DbContext
+    public partial class ChinHookDbContext :  IdentityDbContext<ChinookUser>
     {
 
         public ChinHookDbContext(DbContextOptions<ChinHookDbContext> options)
--- ChinHookInfranstructure/Migrations/20231010091921_Authenticationdata.Designer.cs
+++ ChinHookInfranstructure/Migrations/20231010091921_Authenticationdata.Designer.cs
@@ -4,6 +4,7 @@ using ChinHookInfranstructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChinHookInfranstructure.Migrations
 {
     [DbContext(typeof(ChinHookDbContext))]
-    partial class ChinHookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231010091921_Authenticationdata")]
+    partial class Authenticationdata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
